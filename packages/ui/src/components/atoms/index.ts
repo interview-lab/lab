829003a09a05ee78ab9e60fad85245d3d9435688
BHyeonKim <rlaqhguse@gmail.com>
2026-01-06 22:12:33 +0900

    feat(ui): 컴포넌트 배럴 파일 추가
    atoms와 typographies 컴포넌트를
    그룹화하여 export하는 배럴 파일을 추가했습니다.
    
    - atoms: Badge, Icon
    - typographies: Button, Subtitle, Base, Caption, Title
    - 메인 index.ts에서 re-export

---
+++ packages/ui/src/components/atoms/index.ts
@@ -0,0 +1,9 @@
+import Badge from './Badge';
+import Icon from './Icon';
+
+const Atom = {
+	Badge,
+	Icon,
+};
+
+export default Atom;
---
+++ packages/ui/src/components/typographies/index.ts
@@ -0,0 +1,15 @@
+import Base from './Base';
+import Button from './Button';
+import Caption from './Caption';
+import Subtitle from './Subtitle';
+import Title from './Title';
+
+const Typography = {
+	Button,
+	Subtitle,
+	Base,
+	Caption,
+	Title,
+};
+
+export default Typography;
