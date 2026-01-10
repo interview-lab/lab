9b8001d2a611043b29b74b44a64ac7221bd49321
snowari <70631889+snowari@users.noreply.github.com>
2026-01-11 00:25:21 +0900

    refactor(ui): InputWithValidation import 절대 경로로 변경

--- packages/ui/src/components/molecules/inputWithValidation/inputWithValidation.stories.ts
+++ packages/ui/src/components/molecules/inputWithValidation/inputWithValidation.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
-import * as SvgIcons from '../../../assets/svgs';
+import * as SvgIcons from '@/assets/svgs';
 import InputWithValidation from './InputWithValidation';
 
 const meta = {
