a5723cdaf9a0cc53a3a6739c9e5dd795d359384b
BHyeonKim <rlaqhguse@gmail.com>
2026-01-14 16:23:23 +0900

    refactor(backend): 미사용 AppController, AppService 제거

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from '@/app.controller';
-import { AppService } from '@/app.service';
 import configuration from '@/config/configuration';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
@@ -16,7 +14,7 @@ import { UsersModule } from './users/users.module';
 		UsersModule,
 		AuthModule,
 	],
-	controllers: [AppController],
-	providers: [AppService],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
