f0fd535cb3845062c52db00ddb41d6516fd1da5d
BHyeonKim <rlaqhguse@gmail.com>
2026-02-03 02:34:32 +0900

    feat(ui): SubjectBadge 타입별 색상 스타일 추가
    7개 과목 타입별 data-type 셀렉터 기반
    색상 매핑 구현 및 스토리북 스토리 추가

--- packages/ui/src/components/molecules/index.ts
+++ packages/ui/src/components/molecules/index.ts
@@ -1,9 +1,11 @@
 export { default as DashboardCard } from './DashboardCard';
-export { default as Header, navLinkStyle } from './Header';
+export { default as Header } from './Header';
 export { default as InputPassword } from './InputPassword';
 export { default as InputWithValidation } from './InputWithValidation';
 export { default as InterviewAnswerResultCard } from './InterviewAnswerResultCard';
 export { default as InterviewScoreCard } from './InterviewScoreCard';
 export { default as InterviewSubmitButton } from './InterviewSubmitButton';
+export { default as MessageRecording } from './MessageRecording';
 export { default as SelectSubjectCard } from './SelectSubjectCard';
+export { default as SubjectBadge } from './SubjectBadge';
 export { default as Toggle } from './Toggle';
